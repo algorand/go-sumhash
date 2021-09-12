32b8449722c8b0d9e5b076dae97c05ba821664e8
David Lazar <lazard@csail.mit.edu>
2021-09-12 16:07:34 -0400

    Add comments and readability improvements suggested by Chris P

--- hash.go
+++ hash.go
@@ -71,8 +71,8 @@ func sumBits(as []uint64, b byte) uint64 {
 }
 
 type Compressor interface {
-	Compress(dst []uint64, msg []byte)
-	InputLen() int  // len(msg)
+	Compress(dst []uint64, input []byte)
+	InputLen() int  // len(input)
 	OutputLen() int // len(dst)
 }
 
@@ -121,13 +121,13 @@ func (A LookupTable) Compress(dst []uint64, msg []byte) {
 // digest implementation is based on https://cs.opensource.google/go/go/+/refs/tags/go1.16.6:src/crypto/sha256/sha256.go
 type digest struct {
 	c         Compressor
-	size      int
-	blockSize int
+	size      int // number of bytes in a hash output
+	blockSize int // number of bytes in an input block, per compression
 
-	h   []uint64
-	x   []byte
-	nx  int
-	len uint64
+	h   []uint64 // hash chain (from last compression, or IV)
+	x   []byte   // data written since last compression
+	nx  int      // number of input bytes written since last compression
+	len uint64   // total number of input bytes written overall
 
 	salt []byte // salt block
 }
@@ -154,7 +154,7 @@ func New(c Compressor, salt []byte) hash.Hash {
 
 func (d *digest) Reset() {
 	for i := range d.h {
-		d.h[i] = 0
+		d.h[i] = 0 // all-zeros initialization vector
 	}
 	d.nx = 0
 	d.len = 0
@@ -172,12 +172,12 @@ func (d *digest) BlockSize() int { return d.blockSize }
 
 func (d *digest) Write(p []byte) (nn int, err error) {
 	nn = len(p)
-	// Check if the new length (in bits) overflows.
+	// Check if the new length (in bits) overflows our counter capacity.
 	if uint64(nn) >= (1<<61)-d.len {
 		panic("digest length overflows")
 	}
 	d.len += uint64(nn)
-	if d.nx > 0 {
+	if d.nx > 0 { // continue with existing buffer, if nonempty
 		n := copy(d.x[d.nx:], p)
 		d.nx += n
 		if d.nx == d.blockSize {
@@ -186,12 +186,12 @@ func (d *digest) Write(p []byte) (nn int, err error) {
 		}
 		p = p[n:]
 	}
-	if len(p) >= d.blockSize {
+	if len(p) >= d.blockSize { // handle any remaining full input blocks
 		n := len(p) / d.blockSize * d.blockSize
 		blocks(d, p[:n])
 		p = p[n:]
 	}
-	if len(p) > 0 {
+	if len(p) > 0 { // handle any remaining input
 		d.nx = copy(d.x[:], p)
 	}
 	return
@@ -224,7 +224,8 @@ func (d *digest) checkSum() []byte {
 	var B uint64 = uint64(d.blockSize)
 	var P uint64 = B - 16
 
-	len := d.len << 3
+	bitlen := d.len << 3 // number of input bits written
+
 	// Padding. Add a 1 bit and 0 bits until P bytes mod B.
 	tmp := make([]byte, B)
 	tmp[0] = 0x80
@@ -234,12 +235,13 @@ func (d *digest) checkSum() []byte {
 		d.Write(tmp[0 : B+P-d.len%B])
 	}
 
-	// Length in bits. Note: sumhash uses 128 bits to represent the length.
-	binary.LittleEndian.PutUint64(tmp[0:], len)
-	binary.LittleEndian.PutUint64(tmp[8:], 0) // upper 64 bits are always zero, because len variable has type uint64
+	// Write length in bits, using 128 bits (16 bytes) to represent it.
+	// The upper 64 bits are always zero, because bitlen has type uint64.
+	binary.LittleEndian.PutUint64(tmp[0:], bitlen)
+	binary.LittleEndian.PutUint64(tmp[8:], 0)
 	d.Write(tmp[0:16])
 
-	if d.nx != 0 {
+	if d.nx != 0 { // buffer must be empty now
 		panic("d.nx != 0")
 	}
 
@@ -250,6 +252,7 @@ func (d *digest) checkSum() []byte {
 	return digest
 }
 
+// blocks hashes full blocks of data. len(data) must be a multiple of d.blockSize.
 func blocks(d *digest, data []byte) {
 	cin := make([]byte, d.c.InputLen())
 	block := cin[d.size : d.size+d.blockSize]
