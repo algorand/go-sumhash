450eb65dfd8c7581baa18b5ec10f6b8db9da493c
David Lazar <lazard@csail.mit.edu>
2021-09-12 16:10:10 -0400

    Add expected output tests
    The expected values are generated from the current Go code.

--- hash_test.go
+++ hash_test.go
@@ -3,8 +3,11 @@ package sumhash
 import (
 	"bytes"
 	"crypto/rand"
+	"encoding/hex"
 	"reflect"
 	"testing"
+
+	"golang.org/x/crypto/sha3"
 )
 
 func TestCompression(t *testing.T) {
@@ -42,6 +45,39 @@ func TestCompression(t *testing.T) {
 		if !reflect.DeepEqual(dst1, dst2) {
 			t.Fatalf("compressed outputs differ")
 		}
+	}
+}
+
+func TestExpectedOutput(t *testing.T) {
+	A, err := RandomMatrixFromSeed([]byte("Algorand"), 10, 10*64*2)
+	if err != nil {
+		panic(err)
+	}
+	h := New(A, nil)
+
+	input := make([]byte, 6000)
+	v := sha3.NewShake256()
+	v.Write([]byte("sumhash input"))
+	v.Read(input)
+
+	h.Write(input)
+	sum := h.Sum(nil)
+	expectedSum := "cedae6c2ac201c6d79b5f8af41ceee8d9506adda4f79ab697aed9865773be0912313c6b28b696b219d512b245103830d3e33e541f702d4b9b0395c2dc54781aec9c83c8725e4ee7a608092847d32f037"
+	if hex.EncodeToString(sum) != expectedSum {
+		t.Fatalf("got %x, want %s", sum, expectedSum)
+	}
+
+	salt := make([]byte, BlockSize(A))
+	v.Reset()
+	v.Write([]byte("sumhash salt"))
+	v.Read(salt)
+
+	hs := New(A, salt)
+	hs.Write(input)
+	saltedSum := hs.Sum(nil)
+	expectedSaltedSum := "18ff67b5a2f6f864cd046845f036d2a2be5e91c0324610fdf48921c71382decfdba1c0f619b190953f46c9bb68fb4483300af30f86a62fec384f8c9f4ed6da2debaeec681240362ce7c872cd4b82cad1"
+	if hex.EncodeToString(saltedSum) != expectedSaltedSum {
+		t.Fatalf("got %x, want %s", sum, expectedSum)
 	}
 }
 
