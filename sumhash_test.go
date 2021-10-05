1f768363ee8e0455f1cac57add3458ef89d2f0b0
David Lazar <lazard@csail.mit.edu>
2021-10-05 13:59:19 -0400

    Fix file names

--- sumhash_test.go
+++ sumhash_test.go
@@ -4,13 +4,52 @@ import (
 	"bytes"
 	"crypto/rand"
 	"encoding/hex"
+	"reflect"
 	"testing"
 
 	"golang.org/x/crypto/sha3"
 )
 
+func TestCompression(t *testing.T) {
+	n := 14
+	m := n * 64 * 2
+	A, err := RandomMatrix(rand.Reader, n, m)
+	if err != nil {
+		t.Fatal(err)
+	}
+	At := A.LookupTable()
+
+	if A.InputLen() != m/8 {
+		t.Fatalf("unexpected input len (A): got %d, want %d", A.InputLen(), m/8)
+	}
+	if At.InputLen() != m/8 {
+		t.Fatalf("unexpected input len (At): got %d, want %d", At.InputLen(), m/8)
+	}
+
+	if A.OutputLen() != n*8 {
+		t.Fatalf("unexpected output len (A): got %d, want %d", A.OutputLen(), n*8)
+	}
+	if At.OutputLen() != n*8 {
+		t.Fatalf("unexpected output len (At): got %d, want %d", At.OutputLen(), n*8)
+	}
+
+	dst1 := make([]byte, A.OutputLen())
+	dst2 := make([]byte, A.OutputLen())
+	msg := make([]byte, A.InputLen())
+
+	count := 1000
+	for i := 0; i < count; i++ {
+		rand.Read(msg)
+		A.Compress(dst1, msg)
+		At.Compress(dst2, msg)
+		if !reflect.DeepEqual(dst1, dst2) {
+			t.Fatalf("compressed outputs differ")
+		}
+	}
+}
+
 func TestExpectedOutput(t *testing.T) {
-	A, err := RandomMatrixFromSeed([]byte("Algorand"), 10, 10*64*2)
+	A, err := RandomMatrixFromSeed([]byte("Algorand"), 8, 1024)
 	if err != nil {
 		panic(err)
 	}
@@ -23,9 +62,9 @@ func TestExpectedOutput(t *testing.T) {
 
 	h.Write(input)
 	sum := h.Sum(nil)
-	expectedSum := "cedae6c2ac201c6d79b5f8af41ceee8d9506adda4f79ab697aed9865773be0912313c6b28b696b219d512b245103830d3e33e541f702d4b9b0395c2dc54781aec9c83c8725e4ee7a608092847d32f037"
-	if hex.EncodeToString(sum) != expectedSum {
-		t.Errorf("got %x, want %s", sum, expectedSum)
+	expectedSum := "1ad6dafe03f330e06554300ecc24a59d41ec6afe387c34f4d9a2d971e71ae751823f520135cdc766ba7886a0a2a8954fd17ecae64f58e4431e572571e0f0a9aa"
+	if s := hex.EncodeToString(sum); s != expectedSum {
+		t.Fatalf("got %s, want %s", s, expectedSum)
 	}
 
 	salt := make([]byte, BlockSize(A))
@@ -36,9 +75,9 @@ func TestExpectedOutput(t *testing.T) {
 	hs := New(A, salt)
 	hs.Write(input)
 	saltedSum := hs.Sum(nil)
-	expectedSaltedSum := "18ff67b5a2f6f864cd046845f036d2a2be5e91c0324610fdf48921c71382decfdba1c0f619b190953f46c9bb68fb4483300af30f86a62fec384f8c9f4ed6da2debaeec681240362ce7c872cd4b82cad1"
-	if hex.EncodeToString(saltedSum) != expectedSaltedSum {
-		t.Errorf("got %x, want %s", saltedSum, expectedSaltedSum)
+	expectedSaltedSum := "bc0f4251957352da5102970a32ecad694d88e9f9c4230a2b13d2c7037107245e64e1f7e7dbeca625e2f7d1cd5f63d9070e0255b687301ade29fab952dd44abc7"
+	if s := hex.EncodeToString(saltedSum); s != expectedSaltedSum {
+		t.Fatalf("got %s, want %s", s, expectedSaltedSum)
 	}
 }
 
@@ -50,7 +89,7 @@ func TestHash(t *testing.T) {
 func testHashParams(t *testing.T, n int, m int) {
 	A, err := RandomMatrix(rand.Reader, n, m)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	At := A.LookupTable()
 
@@ -58,10 +97,10 @@ func testHashParams(t *testing.T, n int, m int) {
 	h2 := New(At, nil)
 
 	if h1.Size() != n*8 || h1.BlockSize() != m/8-n*8 {
-		t.Errorf("h1 has unexpected size/blocksize values")
+		t.Fatalf("h1 has unexpected size/blocksize values")
 	}
 	if h2.Size() != n*8 || h2.BlockSize() != m/8-n*8 {
-		t.Errorf("h2 has unexpected size/blocksize values")
+		t.Fatalf("h2 has unexpected size/blocksize values")
 	}
 
 	for _, l := range []int{1, 64, 100, 128, A.InputLen(), 6000, 6007} {
@@ -81,10 +120,41 @@ func testHashParams(t *testing.T, n int, m int) {
 		d2 := h2.Sum(nil)
 
 		if !bytes.Equal(d1, d2) {
-			t.Errorf("matrix and lookup table hashes differ")
+			t.Fatalf("matrix and lookup table hashes differ")
 		}
 
 		h1.Reset()
 		h2.Reset()
+	}
+}
+
+func BenchmarkMatrix(b *testing.B) {
+	A, err := RandomMatrix(rand.Reader, 14, 14*64*2)
+	if err != nil {
+		b.Fatal(err)
+	}
+	msg := make([]byte, A.InputLen())
+	dst := make([]byte, A.OutputLen())
+	rand.Read(msg)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		A.Compress(dst, msg)
+	}
+}
+
+func BenchmarkLookupTable(b *testing.B) {
+	A, err := RandomMatrix(rand.Reader, 14, 14*64*2)
+	if err != nil {
+		b.Fatal(err)
+	}
+	At := A.LookupTable()
+	msg := make([]byte, A.InputLen())
+	dst := make([]byte, A.OutputLen())
+	rand.Read(msg)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		At.Compress(dst, msg)
 	}
 }
